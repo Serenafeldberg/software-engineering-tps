d57fb37173a34164f01eeaed9607af554094e666
sdcarrillo <scarrillo@udesa.edu.ar>
2025-05-25 23:50:23 -0300

    terminado

--- tp3/UNO/src/uno/UnoTest.java
+++ tp3/UNO/src/uno/UnoTest.java
@@ -1,14 +1,11 @@
 package uno;
 
-//import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UnoTest {
